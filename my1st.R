efba361e6883553c7fca8ae502476805032d6de3
1000moutain <wenxiaomin@gmail.com>
2020-01-29 13:35:30 -0800

    then

---
+++ my1st.R
@@ -0,0 +1,17 @@
+
+2^3 # exponents, ^ is preffered, 2**3 works too
+11 %/% 3 #integer division
+11 %% 3 #integer division
+
+snake_case <- "foo_bar"
+dot.case <- "Foo .bar"
+
+#package management
+installed.packages()
+install.packages("ggplot2")
+update.packages()
+remove.packages()
+paste(1,2)
+paste(c("one","two"), c(111,222), sep="___", collapse="++++")
+paste(c("one","two","three"),c("X","Y","z","d"))
+paste(c("one","two","three"),c("X"))
